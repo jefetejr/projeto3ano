73e602bbcf1431bb33a626c9a3fa104aa006af34
jefetejr <161032140+jefetejr@users.noreply.github.com>
2024-03-19 09:27:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const botoes = document.queryselectorall("botao");
+const botoes = document.querySelectorAll("botao");
 
-for (let I =0; i < botoes.length; i++){
+for (let i =0; i < botoes.length; i++){
   botoes [i].onclick = function (){
     for (let j = 0; j < botoes.length; j++){
-      botoes[j].classlist.remove("ativo");
+      botoes[j].classList.remove("ativo");
     }
-    botoes[i].classlist.add("ativo")
+    botoes[i].classList.add("ativo")
   }

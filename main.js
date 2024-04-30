4368a82681898cbc0554bbf32fae81601cf20337
jefetejr <161032140+jefetejr@users.noreply.github.com>
2024-04-30 10:52:10 -0300

    .

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 function calculaTempo(tempoObjetivo){
   let tempoAtual = new Date();
-  contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+  let tempoFinal = tempoObjetivo1 - tempoAtual;
   let segundos = Math.floor(tempofinal / 1000);
   let minutos = Math.floor(segundos / 60);
   let horas = Math.floor(minutos / 60);

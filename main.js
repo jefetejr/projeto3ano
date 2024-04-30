be4dc9ade4ceef814740824fc4c5791f01520655
jefetejr <161032140+jefetejr@users.noreply.github.com>
2024-04-30 10:33:02 -0300

    ...

--- main.js
+++ main.js
@@ -12,7 +12,14 @@ for (let i =0; i < botoes.length; i++){
   }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-01T00:00:00");
+const tempoObjetivo1 = new Date("2024-11-20T23:59:59");
+const tempoObjetivo2 = new Date("2024-10-15T23:59:59");
+const tempoObjetivo3 = new Date("2024-09-10T23:59:59");
+const tempoObjetivo4 = new Date("2024-08-05T23:59:59");
+
+const tempos = [
+  tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
+]
 function calculaTempo(tempoObjetivo)(
   let tempoAtual = new Date();
   contadores[0].textContent = tempoObjetivo1 - tempoAtual;
